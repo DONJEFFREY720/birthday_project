d7c960e6f1f5d561e7d5b520d60a17d370cfa9c3
DON JEFFREY <95631932+DONJEFFREY720@users.noreply.github.com>
2023-02-11 11:40:47 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ function new_image()
      fabric.Image.fromURL("BirthdayImage.jpg",function (Img){
           block_image_object =Img;
 
-          block_image_object.scaleToWidth(150);
-          block_image_object.scaleToHeight(140);
+          block_image_object.scaleToWidth(700);
+          block_image_object.scaleToHeight(510);
           block_image_object.set({
                top:0,
                left:0
